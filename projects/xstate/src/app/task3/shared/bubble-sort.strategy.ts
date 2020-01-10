58926fe99c635aec42efe95a2a9bd2b988052ed4
Alexandr Murashkin <canek1702@gmail.com>
2020-01-10 20:27:15 +0000

    fix(task2): bubble-sort

--- projects/xstate/src/app/task3/shared/bubble-sort.strategy.ts
+++ projects/xstate/src/app/task3/shared/bubble-sort.strategy.ts
@@ -6,7 +6,7 @@ export class BubbleSortStrategy implements SortStrategy {
     let compare = 0;
     array = array.slice();
     for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < array.length - 1; j++) {
+      for (let j = 0; j < array.length - i - 1; j++) {
         if (array[j] > array[j + 1]) {
           [array[j], array[j + 1]] = [array[j + 1], array[j]];
           swap++;
